ff947c3ee3afabb37d3c7170ace57a8cd154dcc1
devpetruskeller <dev.petruskeller@gmail.com>
2022-03-11 15:28:35 +0200

    first template commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+const express = require('express');
+const path = require('path');
+
+const staticPath = path.join(process.cwd(), '/');
+// console.log("staticPath:", staticPath)
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use('/images', express.static(staticPath + 'src/public/images/'));
+app.use('/css', express.static(staticPath + 'src/public/css/'));
+app.use('/scss', express.static(staticPath + 'src/public/scss/'));
+app.use('/plugins', express.static(staticPath + 'src/public/plugins/'));
+app.use('/js', express.static(staticPath + 'src/public/js/'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/:pageCalled', function(req, res) {
+    // console.log('retrieving page: ' + req.params.pageCalled);
+    res.sendFile(path.join(__dirname, `/public/${req.params.pageCalled}`));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
